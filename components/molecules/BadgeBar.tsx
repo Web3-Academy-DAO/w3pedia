691121fcf3e9370f7658081a5fa7c6435176a674
Philip <philip.cn@gmail.com>
2022-08-03 21:07:49 +0800

    Added Badge Bar component (name/count)
    The following files/components are changed:
    
    + added badge bar component (BadgeBar.tsx)
    + added badge bar item component (BadgeItem.tsx)
    + updated landing page to include sample badge bar

---
+++ components/molecules/BadgeBar.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+import { BadgeItem, BadgeItemData } from "../atoms/BadgeItem"
+
+interface BadgeBarProps {
+  data?: BadgeItemData[],
+  value?: BadgeItemData,
+  onChange?: (value: BadgeItemData) => void
+};
+
+const BadgeBar: FC<BadgeBarProps> = ({ data = [], value, onChange = () => { } }: BadgeBarProps) => {
+  return (
+    <div className="container flex flex-row flex-nowrap overflow-auto md:flex-wrap gap-4">
+      {data.map((item, index) => (
+        <BadgeItem data={item} key={index} onClick={() => onChange(item)} active={value?.value == item.value} />
+      ))}
+    </div>
+  )
+}
+
+export { BadgeBar }
+export type { BadgeBarProps }
